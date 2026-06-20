8e9e7f93f55a4d83f3735c5a15cbb21a9937ce2d
Thomas Vérin <thomas.verin@sonarsource.com>
2026-06-20 05:53:16 +0200

    Update BRM 1000 race PWA (2026-06-20)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_APP = 'c555-app-v1780753351';
+const CACHE_APP = 'c555-app-v1781927595';
 const CACHE_TILES = 'c555-tiles-v1';
 const APP_SHELL = ["./index.html", "./manifest.webmanifest", "./aide.html"];
 
